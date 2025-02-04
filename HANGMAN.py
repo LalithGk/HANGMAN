6dd0ea8c816282f3dfa844ba64ae83be0ac6e7bc
Lalith gk <LalithGk>
2025-02-04 15:14:23 +0530

    While loop

--- HANGMAN.py
+++ HANGMAN.py
@@ -2,13 +2,24 @@ import random
 names = ["Alice", "Bob", "Charlie", "David", "Emma"]
 a = random.choice(names).lower()
 print(a)
-blank = " "
 
-b = input("Enter your trail: ").lower()
-for i in a :
-    if i == b:
-        blank = blank + i
-    else:
-        blank = blank + "_"
+trail = False
+correct = []
+while not trail:
+    b = input("Enter your trail: ").lower()
+    blank = ""
+
     
-print(blank)
+    for i in a :
+        if i == b:
+            blank = blank + i
+            correct.append(i)
+        elif i in correct:
+            blank = blank + i
+            
+        else:
+            blank = blank + "_"
+    print(blank)
+    if "_" not in blank:
+        trail = True 
+        print("You won") 
