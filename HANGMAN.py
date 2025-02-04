4728ecb4458cb140a6f0de8be4198fb6abefab51
Lalith gk <LalithGk>
2025-02-04 14:27:43 +0530

    Making blanks

--- HANGMAN.py
+++ HANGMAN.py
@@ -1,6 +1,14 @@
 import random
 names = ["Alice", "Bob", "Charlie", "David", "Emma"]
-a = random.choice(names)
+a = random.choice(names).lower()
 print(a)
+blank = " "
 
-b = input("Enter your trail: ")
+b = input("Enter your trail: ").lower()
+for i in a :
+    if i == b:
+        blank = blank + i
+    else:
+        blank = blank + "_"
+    
+print(blank)
